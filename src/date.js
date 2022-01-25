7b7377ca6c2b7541985ae5ba3dc88290022992bc
Christian Harvey <hikusabi@gmail.com>
2022-01-25 20:24:18 +0000

    Small tweak to isLeapYear to see if it shows proper code coverage afterwards

--- src/date.js
+++ src/date.js
@@ -48,7 +48,10 @@ Date.prototype.addSeconds = function (seconds) {
  * @returns {boolean}
  */
 Date.isLeapYear = function (year) {
-    return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    if (year % 400 === 0) {
+        return true;
+    }
+    return year % 4 === 0 && year % 100 !== 0;
 }
 
 /**
